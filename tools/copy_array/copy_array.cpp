211712794f13002179073687924063bf1072a787
anditty <71593976+Anditty@users.noreply.github.com>
2021-05-26 21:09:16 +0800

    add copy_array.cpp

---
+++ tools/copy_array/copy_array.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 詹子正 on 2021/5/26.
+//
+
+#include "copy_array.h"
+
+char** copy(char **command){
+    char** out;
+    while (*command!=","){
+
+    }
+}
---
+++ tools/copy_array/copy_array.h
@@ -0,0 +1,9 @@
+//
+// Created by 詹子正 on 2021/5/26.
+//
+#include <cstring>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+char** copy(char **command);
