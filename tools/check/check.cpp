b08d3c99bd549f8332c23d73c8455781c9b96fa5
edison <735318617@qq.com>
2021-05-29 20:06:31 +0800

    version 0.1.2 update min distance

--- tools/check/check.cpp
+++ tools/check/check.cpp
@@ -27,49 +27,41 @@ int min_modify_dist(string word1, string word2) {
     int n = word1.length();
     int m = word2.length();
 
-    // 有一个字符串为空串
     if (n * m == 0) return n + m;
 
-    // DP 数组
-    int D[n + 1][m + 1];
-
-    // 边界状态初始化
-    for (int i = 0; i < n + 1; i++) {
-        D[i][0] = i;
+    int opt[n + 1][m + 1];
+    for (int i = 0; i <= n; i++) {
+        opt[i][0] = i;
     }
-    for (int j = 0; j < m + 1; j++) {
-        D[0][j] = j;
+    for (int j = 0; j <= m; j++) {
+        opt[0][j] = j;
     }
 
-    // 计算所有 DP 值
-    for (int i = 1; i < n + 1; i++) {
-        for (int j = 1; j < m + 1; j++) {
-            int left = D[i - 1][j] + 1;
-            int down = D[i][j - 1] + 1;
-            int left_down = D[i - 1][j - 1];
-            if (word1[i - 1] != word2[j - 1]) left_down += 1;
-            D[i][j] = min(left, min(down, left_down));
-
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (word1[i - 1] == word2[i - 1]) {
+                opt[i][j] = opt[i - 1][j - 1];
+            } else {
+                opt[i][j] = min(min(opt[i - 1][j - 1], opt[i][j - 1]), opt[i - 1][j]) + 1;
+            }
         }
     }
-    return D[n][m];
+    return opt[n][m];
 }
 
 
-bool check_is_num(const string& str)
-{
+bool check_is_num(const string &str) {
     stringstream sin(str);
     int c;
 
-    if (sin >> c)
-    {
+    if (sin >> c) {
         return true;
     }
 
     return false;
 }
 
-bool check_command_args(int min_args, char **command){
+bool check_command_args(int min_args, char **command) {
     for (int i = 1; i <= min_args; ++i) {
         if (command[i] == nullptr) {
             return false;
